c77e4f1d51c66b843fdcdaf90090dbbfe51a7c2e
Alex Haumer <alexander.haumer@me.com>
2022-02-02 22:40:26 +0000

    Goals CRUD

---
+++ app/controllers/goals_controller.rb
@@ -0,0 +1,52 @@
+class GoalsController < ApplicationController
+
+  before_action :set_goal, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @goals = current_user.goals
+  end
+
+  def show
+  end
+
+  def new
+    @goal = Goal.new
+  end
+
+  def create
+    @goal = Goal.create(goal_params)
+    @goal.user = current_user
+
+    if @goal.save
+      redirect_to @goal
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @goal.update(goal_params)
+      redirect_to @goal
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @goal.destroy
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def set_goal
+    @goal = Goal.find(params[:id])
+  end
+
+  def goal_params
+    params.require(:goal).permit(:name, :description, :due_date)
+  end
+end
