c7e7c70784e1687530de83b76199665ff2830d88
Benjamin Baranger <baranger.benjamin@gmail.com>
2022-02-04 22:49:14 +0000

    implicit self

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -2,5 +2,5 @@ class Goal < ApplicationRecord
   has_many :user_goals
   has_many :users, through: :user_goals
 
-  scope :not_completed, ->(user) { Goal.joins(:user_goals).merge(UserGoal.where_belongs_to(user)).where(complete: false) }
+  scope :not_completed, ->(user) { joins(:user_goals).merge(UserGoal.where_belongs_to(user)).where(complete: false) }
 end
